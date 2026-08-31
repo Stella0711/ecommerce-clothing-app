c8621a4007042f5627d378313e641efb6c13d1ca
Stella0711 <crystal.quigley@gmail.com>
2026-08-31 09:24:07 -0500

    Build collection filter UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
+import Collection from './Collection'
 
 
 const Home = () => {
@@ -14,6 +15,7 @@ const Home = () => {
         <BestSeller />
         <OurPolicy />
         <NewsletterBox />
+        <Collection />
     </div>
   )
 }
